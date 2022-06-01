e24f9d10d53b1429bbdb7de90a31a609bc9b5981
Tom <demart.tom@gmail.com>
2022-05-31 22:34:16 -0700

    Added navbar with routing to template pages. Moved contact form to contact page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import ContactForm from './ContactForm';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Info from "./pages/info";
+import ContactForm from "./ContactForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ContactForm />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+    </Router>
   );
 }
 
