38dee9a62261acd08cb1f35f2d165398472cdc6e
Tom <demart.tom@gmail.com>
2022-06-03 21:58:18 -0700

    Updating navbar/header

--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -2,12 +2,45 @@ import { FaBars } from "react-icons/fa";
 import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
+export const Head = styled.header`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: -0.5em;
+  width: 35em;
+  text-align: right;
+  font-size: 36px;
+  height: 175px;
+  position: relative;
+`;
+
+export const LogoBox = styled.div`
+  text-align: left;
+  float: left;
+  position: absolute;
+  top: 1em;
+  bottom: 0;
+  left: 0;
+`;
+
+export const Logo = styled.img`
+  object-fit: contain;
+  width: 70%;
+`;
+
+export const Name = styled.div`
+  text-align: right;
+  float: right;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
 export const Nav = styled.nav`
-  background: #ffb3ff;
+  background: #ffffff;
   height: 85px;
   display: flex;
-  justify-content: space-between;
-  padding: 0.2rem calc((100vw - 1000px) / 2);
+  justify-content: end;
+  padding: 0.2rem calc((90vw - 1000px) / 2);
   z-index: 12;
 `;
 
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { Nav, NavLink, NavMenu } from "./NavbarElements";
+import {
+  Head,
+  LogoBox,
+  Logo,
+  Name,
+  Nav,
+  NavLink,
+  NavMenu,
+} from "./NavbarElements";
+import logo from "../../../src/images/plant_path_text_only1_copy_600x273.jpg";
 
 const Navbar = () => {
   return (
     <>
+      <Head>
+        <LogoBox>
+          <Logo src={logo} alt="Plant Path Wellness logo" />
+        </LogoBox>
+        <Name>
+          <span style={{ color: "orange" }}>Patty DeMartino, </span>
+          <span style={{ color: "green" }}>NBC-HWC</span>
+        </Name>
+      </Head>
       <Nav>
         <NavMenu>
           <NavLink to="/">Home</NavLink>
