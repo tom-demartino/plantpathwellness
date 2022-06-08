20c25329d0f8da0a1f4c1de37526be8449ae6f40
Tom <demart.tom@gmail.com>
2022-06-08 11:42:11 -0700

    Adding router basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/plantpathwellness-website">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
