70c93676b2edaf41e2b07ca857f5efc4c603ca5f
Tom <demart.tom@gmail.com>
2022-06-08 10:36:55 -0700

    Adding basename for router for gh pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="plantpathwellness-website">
       <Header />
       <Switch>
         <Route path="/" element={<Home />} />
