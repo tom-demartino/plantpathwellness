339f0626c5eb18b9e6f6f2f96facf8c5073d8ecd
Tom <demart.tom@gmail.com>
2022-06-08 14:44:20 -0700

    exact path for homepage

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/info" element={<Info />} />
