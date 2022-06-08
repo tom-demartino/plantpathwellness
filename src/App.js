6852eb6c6c0d96a401595c9c031e15b39d9dc17f
Tom <demart.tom@gmail.com>
2022-06-08 10:36:01 -0700

    Changing to hashrouter for github pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
 import About from "./pages/about";
 import Contact from "./pages/contact";
@@ -12,12 +13,12 @@ function App() {
   return (
     <Router>
       <Header />
-      <Routes>
+      <Switch>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/info" element={<Info />} />
-      </Routes>
+      </Switch>
       <Footer />
     </Router>
   );
