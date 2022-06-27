8e29a396c021422608a89ff3f4a5054e3d2dd7d1
Tom <demart.tom@gmail.com>
2022-06-27 16:31:01 -0700

    Fixing up lambda function to include dependencies

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,6 +5,9 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "plantpathwellnessnodemailerDependencies": {
+            "Arn": "string"
         }
     },
     "api": {
