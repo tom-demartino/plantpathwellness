1447a533cbeda9a88fb2585c7eb1aa937500c926
Tom <demart.tom@gmail.com>
2022-06-28 14:50:46 -0700

    Removing comment

--- amplify/backend/function/nodemailer/src/app.js
+++ amplify/backend/function/nodemailer/src/app.js
@@ -1,17 +1,3 @@
-/*
-Use the following code to retrieve configured secrets from SSM:
-
-const aws = require('aws-sdk');
-
-const { Parameters } = await (new aws.SSM())
-  .getParameters({
-    Names: ["OAUTH2_CLIENT_ID","OAUTH2_CLIENT_SECRET","OAUTH2_REFRESH_TOKEN"].map(secretName => process.env[secretName]),
-    WithDecryption: true,
-  })
-  .promise();
-
-Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
-*/
 const express = require("express");
 const bodyParser = require("body-parser");
 const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
